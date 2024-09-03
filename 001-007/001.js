7a0bdc4d1f8d4de882526cd341c70218d1b199f1
youssefsafwat234 <yousefsafwat367@gmail.com>
2024-09-03 20:12:15 +0300

    Javascript usage

---
+++ 001-007/001.js
@@ -0,0 +1,9 @@
+/*
+    TODO: javascript used in 
+        -> front end (with html ,  css or framework like reactJs or VueJs)
+        -> backend (nodeJs)
+        -> iot (in a cross platform  used with framework like iotJs and cylonJs)
+        -> mobile application (used react native in cross platform)
+        -> desktop application ( in cross platform) 
+    TODO: javascript use for making a dynamic page
+*/
